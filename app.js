4a3964a996c9e4d8eea0e7e1e31f4a09a17bd076
Muhammad Rizwan <mrizwanmanzoor111@gmail.com>
2026-04-20 03:08:51 -0700

    feature added

--- app.js
+++ app.js
@@ -2,21 +2,31 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 // my code  here  yes
+//jwt
+app.use(cookieParser()); 
+app.get('/', (req, res) => {
+    let token = jwt.sign({email: "mrizwan@gmail.com"}, "secret");
+    res.cookie("jwt", token);
+    res.send("done");
+});
+app.get('/read', (req, res) => {
+    let data= jwt.verify(req.cookies.jwt, "secret");
+    console.log(data);
+  });
 
-app.use(cookieParser());    
-
+   //cookies
 app.get('/', (req, res) => {
   res.cookie('name', 'Rizwan');
   res.send('Cookie has been set!');
 });
 
 app.get('/read', (req, res) => {
-    console.log(req.cookies); // Assuming you have cookie-parser middleware set up     
-
+    console.log(req.cookies); // Assuming you have cookie-parser middleware set up    
   res.send("read cookie");
 });
-
+//bcrypt
 app.get("/", function (req, res) {
     bcrypt.genSalt(10, function(err, salt) {
    bcrypt.hash('rizwan', salt, function(err, hash) {
@@ -30,7 +40,7 @@ bcrypt.compare('rizwan','$2b$10$CGbcDmE8wxtW92h/Y/FgBO5K3TGdfPweGlgJlhYnvVj.YTAs
 });
                       
 
-
+//port
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
